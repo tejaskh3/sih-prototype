95651165b53b72de9798d85776cf7aa6558189f8
TEJAS <tejaskh3@gmail.com>
2023-09-07 11:35:39 +0530

    added firebase db and auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route, Router } from 'react-router-dom';
-
+import { signInWithGooglePopup } from './utils/firebase/firebase';
 const App = () => {
   return (
     <>
-    <h1>Sih Prototpe</h1>
-      <Router></Router>
+      <h1>Sih Prototpe</h1>
+      <button onClick={signInWithGooglePopup}>sign in</button>
     </>
   );
 };

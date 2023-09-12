8d816965ddc96db6544718468ff33b554ce1b9ea
sagarb2003 <sagarssb2003@gmail.com>
2023-09-12 23:53:19 +0530

    developed advocates component

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,43 @@ import ServiceProvidersHome from './pages/service-providers/ServiceProvidersHome
 import ServiceProviderSignInForm from './Components/ServiceProviderSignIn.component';
 import ServiceProviderRegisterForm from './Components/ServiceProviderRegisterForm.component';
 import UserSignInForm from './Components/UserSignInForm.component';
+import Advocates from './components/Advocates.component';
 // UserSignInForm;
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />
-        },
-        {
-          index: true,
-          element: <HomePage />
-        },
-        {
-          path: 'service-providers-home',
-          element: <ServiceProvidersHome />
-        },
-        { path: 'auth', element: <ServiceProviderSignInForm/> },
-        { path: 'auth/sign-up', element: <ServiceProviderRegisterForm/> },
-        { path: 'auth/user', element: <UserSignInForm/> }
-      ]
-    }
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element: <RootLayout />,
+  //     children: [
+  //       {
+  //         index: true,
+  //         element: <HomePage />
+  //       },
+  //       {
+  //         index: true,
+  //         element: <HomePage />
+  //       },
+  //       {
+  //         path: 'service-providers-home',
+  //         element: <ServiceProvidersHome />
+  //       },
+  //       { path: 'auth', element: <ServiceProviderSignInForm/> },
+  //       { path: 'auth/sign-up', element: <ServiceProviderRegisterForm/> },
+  //       { path: 'auth/user', element: <UserSignInForm/> }
+  //     ]
+  //   }
+  // ]);
 
-  return (
-    <>
-      <RouterProvider router={router}></RouterProvider>;
-    </>
-  );
+  // return (
+  //   <>
+  //     <RouterProvider router={router}></RouterProvider>;
+  //   </>
+  // );
+  return(
+
+    <Advocates/>
+  )
 };
 
 export default App;
